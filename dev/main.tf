c98cf577835baf8256634369f114b874281bf5ba
sundarbalaji11 <72366797+sundarbalaji11@users.noreply.github.com>
2021-05-08 22:35:13 +0530

    Add files via upload

---
+++ dev/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "my_vpc" {
+  source      = "../modules/vpc"
+  vpc_cidr    = "192.168.0.0/16"
+  tenancy     = "default"
+  vpc_id      = "${module.my_vpc.vpc_id}"
+  subnet_cidr = "192.168.1.0/24"
+}
+
+module "my_ec2" {
+  source        = "../modules/ec2"
+  ec2_count     = 1
+  ami_id        = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.my_vpc.subnet_id}"
+}
